deeb40b82ea5960745a7dace7a1d0bcfe9346fe8
Ernesto Aramayo <ernestoaaramayo@hotmail.com>
2026-05-24 13:01:45 -0300

    correccion inciso 5 y cosas menores

--- trabajo integrador/TAD_Orden/app.py
+++ trabajo integrador/TAD_Orden/app.py
@@ -11,12 +11,12 @@ continuar = True
 #Se genera una lista con datos de prueba
 def datosprueba(ot):
     datos_prueba = [
-        (10001, "Computadora", "Control", "Julian", "06-05-2026", "09:00"),
-        (10002, "Equipo 2", "Control", "Marcos", "06-05-2026", "15:00"),
-        (10003, "Impresora", "Finanzas", "Elias", "06-05-2026", "10:00"),
-        (10004, "Algo", "RRHH", "Hernan", "20-05-2026", "11:00"),
-        (10005, "Maquina", "RRHH", "Sandro", "25-05-2026", "12:00"),
-        (10006, "Algo 222", "Operaciones", "Ezequiel", "26-05-2026", "13:00")
+        (10001, "Computadora", "Control", "Julian", datetime.strptime("05/06/2026", "%d/%m/%Y").date(), "09:00"),
+        (10002, "Equipo 2", "Control", "Marcos", datetime.strptime("05/06/2026", "%d/%m/%Y").date(), "15:00"),
+        (10003, "Impresora", "Finanzas", "Elias", datetime.strptime("06/05/2026", "%d/%m/%Y").date(), "10:00"),
+        (10004, "Algo", "RRHH", "Hernan", datetime.strptime("20/05/2026", "%d/%m/%Y").date(), "11:00"),
+        (10005, "Maquina", "RRHH", "Sandro", datetime.strptime("25/05/2026", "%d/%m/%Y").date(), "12:00"),
+        (10006, "Algo 222", "Operaciones", "Ezequiel", datetime.strptime("26/05/2026", "%d/%m/%Y").date(), "13:00")
     ]
     for id_maquina, nombre_equipo, sector_planta, tecnico, fecha, hora in datos_prueba:
         orden = crearOrden()
@@ -31,12 +31,12 @@ datosprueba(ot)
 while(continuar == True):
     print("\n\n1) Alta de Ordenes de Trabajo")
     print("2) Modificación de Cronograma")
-    print("3) Cancelación de Tareas:")
+    print("3) Cancelación de Tareas")
     print("4) Reporte General de Mantenimiento")
     print("5) Reprogramación por Parada de Planta (Traslado Masivo)")
     print("6) Depuración y Generación de Lista de Prioridad")
     print("7) Salir")
-    opcion = int(input("Ingrese una opcion: "))
+    opcion = int(input("Ingrese una opción: "))
 
     if opcion == 1:
         orden = crearOrden()
@@ -57,7 +57,7 @@ while(continuar == True):
 
         nombre_equipo = input("Ingrese el nombre del equipo: ")
         sector_planta = input("Ingrese el sector de la planta: ")
-        tecnico = input("Ingrese el nombre del tecnnico: ")
+        tecnico = input("Ingrese el nombre del técnico: ")
 
         # Bucle para la Fecha
         while True:
@@ -66,7 +66,7 @@ while(continuar == True):
                 f = input("Ingrese fecha (DD/MM/AAAA): ")
                 fecha_valida = datetime.strptime(f, "%d/%m/%Y").date()
                 if fecha_valida < date.today():
-                    print("Inválido. La fecha es anterior a hoy.")
+                    print("Error. La fecha es anterior a hoy.")
                     continue
                 break
             except ValueError:
@@ -86,17 +86,17 @@ while(continuar == True):
 
         print("-----------------------------")
         print("Generando orden...")
-        print("ID de la maquina: ", id_maquina)
+        print("ID de la máquina: ", id_maquina)
         print("Nombre del equipo: ", nombre_equipo)
         print("Sector de la planta: ", sector_planta)
-        print("Nombre del tecnnico: ", tecnico)
-        print("Fecha: ", fecha_valida)
+        print("Nombre del técnico: ", tecnico)
+        print("Fecha de la orden: ", fecha_valida)
         print("Hora de la orden: ", hora_valida)
         print("-----------------------------")
 
     elif opcion == 2:
         encontrada = False
-        id_maquina = int(input("Ingrese ID de maquina: "))
+        id_maquina = int(input("Ingrese ID de la máquina: "))
         for i in range(0, tamanio(ot)):
             o = recuperarOrden(ot, i)
 
@@ -109,7 +109,7 @@ while(continuar == True):
                         f = input("Ingrese fecha (DD/MM/AAAA): ")
                         nueva_fecha_valida = datetime.strptime(f, "%d/%m/%Y").date()
                         if nueva_fecha_valida < date.today():
-                            print("Inválido. La fecha es anterior a hoy.")
+                            print("Error. La fecha es anterior a hoy.")
                             continue
                         break
                     except ValueError:
@@ -128,10 +128,10 @@ while(continuar == True):
                 break
 
         if not encontrada:
-            print('No se encontro la orden.');
+            print('No se encontró la orden.');
 
     elif opcion == 3:
-        id_maquina = int(input("Ingrese el id de maquina a eliminar: "))
+        id_maquina = int(input("Ingrese el ID de máquina de la orden a eliminar: "))
         i = 0
         encontrada = False
 
@@ -141,7 +141,7 @@ while(continuar == True):
             if verIDMaquina(o) == id_maquina:
                 encontrada = True
                 
-                print("Nombre equipo de orden a eliminar: ", verNombreEquipo(o))
+                print("Nombre del equipo de la orden a eliminar: ", verNombreEquipo(o))
                 eliminarOrden(ot, o)
                 print("Orden eliminada.") 
                 break
@@ -149,7 +149,7 @@ while(continuar == True):
                 i+=1
                 
         if not encontrada:
-            print("No se encontro la orden.")            
+            print("No se encontró la orden.")            
 
     elif opcion == 4:
 
@@ -157,31 +157,31 @@ while(continuar == True):
         for i in range(0, tamanio(ot)):
             o = recuperarOrden(ot, i)
 
-            print("ID: {} | Nombre equipo: {} | Sector: {} | Tecnico: {} | Fecha: {} | Hora: {}".format(verIDMaquina(o), verNombreEquipo(o), verSectorPlanta(o), verTecnicoAsignado(o), verFechaProgramada(o), verHoraInicio(o)))
+            print("ID: {} | Nombre equipo: {} | Sector: {} | Técnico: {} | Fecha: {} | Hora: {}".format(verIDMaquina(o), verNombreEquipo(o), verSectorPlanta(o), verTecnicoAsignado(o), verFechaProgramada(o), verHoraInicio(o)))
 
     elif opcion == 5:
-        fecha_determinada = input("Ingrese la fecha de las ordenes que desea modificar: ")
+        fecha_determinada = datetime.strptime(input("Ingrese la fecha de las órdenes que desea modificar (DD/MM/AAAA): "), "%d/%m/%Y").date()
 
         #Nueva fecha
         while True:
             try:
-                f = input("Ingrese fecha (DD/MM/AAAA): ")
+                f = input("Ingrese la nueva fecha (DD/MM/AAAA): ")
                 nueva_fecha_valida = datetime.strptime(f, "%d/%m/%Y").date()
                 if nueva_fecha_valida < date.today():
-                    print("Inválido. La fecha es anterior a hoy.")
+                    print("Error. La fecha es anterior a hoy.")
                     continue
                 break
             except ValueError:
                 print("Formato de fecha inválido.")
 
         #Nueva hora
-                while True:
-                    try:
-                        h = input("Ingrese hora (HH:MM): ")
-                        nueva_hora_valida = datetime.strptime(h, "%H:%M").time()
-                        break
-                    except ValueError:
-                        print("Formato de hora inválido.")
+        while True:
+            try:
+                h = input("Ingrese la nueva hora (HH:MM): ")
+                nueva_hora_valida = datetime.strptime(h, "%H:%M").time()
+                break
+            except ValueError:
+                print("Formato de hora inválido.")
 
         i = 0
         while i < tamanio(ot):
@@ -190,29 +190,30 @@ while(continuar == True):
             if verFechaProgramada(o) == fecha_determinada:
                 modFechaProgramada(o, nueva_fecha_valida)
                 modHoraInicio(o, nueva_hora_valida)
-                print("Nueva fecha de la orden ", verIDMaquina(o),": ",verFechaProgramada(o))
+                print("Nueva fecha de la orden", verIDMaquina(o),": ",verFechaProgramada(o))
 
             i+=1
 
     elif opcion == 6:
-        print("\na)Baja de ordenes por sector")
+        print("\na)Baja de órdenes por sector")
         print("b)Generar nueva cola de prioridades")
-        opc = input("Ingrese la opcion seleccionada: ")
+        opc = input("Ingrese la opción seleccionada: ")
 
         if opc == 'a':
-            sector_borrar = input("Ingrese el sector de las ordenes que desea eliminar: ")
+            sector_borrar = input("Ingrese el sector de las órdenes que desea eliminar: ")
 
             i = 0
             while i < tamanio(ot):
                 o = recuperarOrden(ot, i)
                 if sector_borrar == verSectorPlanta(o):
                     eliminarOrden(ot, o)
+                    print("Orden eliminada: ", verIDMaquina(o))
                 else:
                     i+=1
         else:
             if opc == 'b':
                 cola = crearCola()
-                dia_especifico = input("Ingrese el dia de las ordenes que desea agregar a la cola(AAAA-MM-DD): ")
+                dia_especifico = input("Ingrese el día de las órdenes que desea agregar a la cola(AAAA-MM-DD): ")
                 #Se encolan las ordenes del dia determinado
                 for i in range(0, tamanio(ot)):
                     o = recuperarOrden(ot, i)
@@ -224,11 +225,11 @@ while(continuar == True):
                 print("\n-----------------------------")
                 while esVacia(cola) != True:
                     elem_cola = desencolar(cola)
-                    print("Nombre equipo: {} | Nombre Tecnico: {}".format(verNombreEquipo(elem_cola), verTecnicoAsignado(elem_cola)))
+                    print("Nombre equipo: {} | Nombre Técnico: {}".format(verNombreEquipo(elem_cola), verTecnicoAsignado(elem_cola)))
                 print("-----------------------------")
 
             else:
-                print('Opcion incorrecta. Intente nuevamente.')
+                print('Opción incorrecta. Intente nuevamente.')
     
     elif opcion == 7:
         print("Saliendo...")
